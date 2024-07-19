ad55ed3e953edd4d4c3c8ecd88bfbe264bd1f14c
LifeIsFiction5 <jacob_barrett@icloud.com>
2024-07-18 22:36:59 -0500

    Finished exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,11 +1,15 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+    try { 
+      result = lab.runLab(3);
+    } catch (e) {
+      result = "Error thrown";
+    }
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
-
 let studentLabs = [
   {
     student: 'Carly',
@@ -21,4 +25,31 @@ let studentLabs = [
   }
 ];
 
+
+let studentLabs2 = [
+
+
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+
 gradeLabs(studentLabs);
+
+gradeLabs(studentLabs2);
