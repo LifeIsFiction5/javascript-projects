482b1de9d9022c9afddab4f9430fa5ab21b552e8
LifeIsFiction5 <jacob_barrett@icloud.com>
2024-08-11 20:42:02 -0500

    Finished exercises for DOM and Events

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener("mouseenter", function ( event ) {
+        event.target.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", function ( event ) {
+        event.target.style.backgroundColor = "";
+    });
+
+    missionAbort.addEventListener('click', function ( event ) {
+        const userConfirmed = confirm("Are you sure you want to abort the mission?");
+            if (userConfirmed) {
+                paragraph.innerHTML = "Mission aborted! Space shuttle returning home.";
+            }
+    });
 }
 
 window.addEventListener("load", init);
